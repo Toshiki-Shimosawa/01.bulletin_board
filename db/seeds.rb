53ce490bca9edd717799ca931a52932ca3e44b71
TOSHIKI SHIMOSAWA <toshiki@TOSHIKInoMacBook-Pro.local>
2020-05-29 00:24:27 +0900

    update for upload to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,11 @@ if Rails.env == 'development'
   (1..50).each do |i|
     Board.create(name:"ユーザー#{i}", title:"タイトル#{i}", body:"本文#{i}")
   end
+
+  Tag.create([
+    {name: 'おすすめ店舗紹介'},
+    {name: '糖質制限お役立ち情報'},
+    {name: 'このサイトに関するご要望'},
+    {name: 'その他'}
+  ])
 end
