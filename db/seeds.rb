649cf8c3967e5471c1e2ceb0db04c63709863b8e
TOSHIKI SHIMOSAWA <toshiki@TOSHIKInoMacBook-Pro.local>
2020-05-29 10:00:17 +0900

    20200529r1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env == 'production'
+  (1..50).each do |i|
+    Board.create(name:"ユーザー#{i}", title:"タイトル#{i}", body:"本文#{i}")
+  end
+
+  Tag.create([
+    {name: 'おすすめ店舗紹介'},
+    {name: '糖質制限お役立ち情報'},
+    {name: 'このサイトに関するご要望'},
+    {name: 'その他'}
+  ])
+end
+
 if Rails.env == 'development'
   (1..50).each do |i|
     Board.create(name:"ユーザー#{i}", title:"タイトル#{i}", body:"本文#{i}")
